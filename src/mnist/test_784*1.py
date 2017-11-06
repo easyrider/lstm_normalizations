5aaebc2ed23e14192946d96f5b6a05d83700ae88
jake <jakewill@qq.com>
2017-11-06 17:34:51 +0800

    refactor

---
+++ src/mnist/test_784*1.py
@@ -0,0 +1,222 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+
+import tensorflow as tf
+from src.normal_cells.lstm_bn_sep import BNLSTMCell
+from src.normal_cells.lstm_cn_scale_input import CNSCALELSTMCell
+from src.normal_cells.lstm_cn_sep import CNLSTMCell
+from src.normal_cells.lstm_ln_sep import LNLSTMCell
+from src.normal_cells.lstm_pcc_sep import PCCLSTMCell
+from src.normal_cells.lstm_wn_sep import WNLSTMCell
+from tensorflow.examples.tutorials.mnist import input_data
+
+from src.normal_cells.lstm_basic import BASICLSTMCell
+
+# Training Parameters
+# learning_rate = 0.001
+# training_steps = 100000
+batch_size = 128
+display_step = 200
+
+# Network Parameters
+num_input = 1  # MNIST data input (img shape: 28*28)
+timesteps = 784  # timesteps
+num_hidden = 128  # hidden layer num of features
+num_classes = 10  # MNIST total classes (0-9 digits)
+
+FLAGS = None
+
+
+def run():
+	mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+
+	cell_dic = {
+		'base': BASICLSTMCell,
+		'bn_sep': BNLSTMCell,
+		'cn_sep': CNLSTMCell,
+		'cn_scale_sep': CNSCALELSTMCell,
+		'ln_sep': LNLSTMCell,
+		'wn_sep': WNLSTMCell,
+		'pcc_sep': PCCLSTMCell
+	}
+
+	# tf Graph input
+	X = tf.placeholder("float", [None, timesteps, num_input])
+	Y = tf.placeholder("float", [None, num_classes])
+	training = tf.placeholder(tf.bool)
+
+	# Define weights
+
+	with tf.name_scope('final_weights'):
+		w = tf.get_variable(
+			'W', [num_hidden, num_classes],
+			initializer=tf.orthogonal_initializer())
+	tf.summary.histogram("f_weight", w)
+
+	with tf.name_scope('final_biases'):
+		b = tf.get_variable('b', [num_classes])
+	tf.summary.histogram("f_biases", b)
+
+	def RNN(x, weights, biases):
+
+		# Prepare data shape to match `rnn` function requirements
+		# Current data input shape: (batch_size, timesteps, n_input)
+		# Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
+
+		# Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
+		x = tf.unstack(x, timesteps, 1)
+		# x = tf.convert_to_tensor(x)
+		if FLAGS.cell != 'bn_sep':
+			# Define a lstm cell with tensorflow
+			init_state = tf.contrib.rnn.LSTMStateTuple(
+				tf.truncated_normal([batch_size, num_hidden], stddev=0.1),
+				tf.truncated_normal([batch_size, num_hidden], stddev=0.1))
+			lstm_cell = cell_dic[FLAGS.cell](num_hidden, forget_bias=1.0)
+
+			# Get lstm cell output
+			outputs, states = tf.nn.static_rnn(
+				lstm_cell, x, initial_state=init_state, dtype=tf.float32)
+
+			# Linear activation, using rnn inner loop last output
+			return tf.matmul(outputs[-1], weights) + biases
+
+		else:
+			init_state = (tf.truncated_normal(
+				[batch_size, num_hidden], stddev=0.1), tf.truncated_normal(
+				[batch_size, num_hidden], stddev=0.1), tf.constant(0.0, shape=[1]))
+			lstm_cell = BNLSTMCell(
+				num_hidden,
+				forget_bias=1.0,
+				max_bn_steps=FLAGS.max_steps,
+				is_training_tensor=training)
+			outputs, states = tf.nn.static_rnn(
+				lstm_cell, x, initial_state=init_state, dtype=tf.float32)
+			_, final_hidden, _ = states
+
+			return tf.matmul(final_hidden, weights) + biases
+
+	logits = RNN(X, w, b)
+
+	prediction = tf.nn.softmax(logits)
+
+	# Define loss and optimizer
+	loss_op = tf.reduce_mean(
+		tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+	tf.summary.scalar("xe_loss", loss_op)
+
+	correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+	accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+	tf.summary.scalar('acc', accuracy)
+
+	optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.lr)
+	gvs = optimizer.compute_gradients(loss_op)
+	capped_gvs = [(None if grad is None else tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
+
+	train_op = optimizer.apply_gradients(capped_gvs)
+
+	for (grad, var), (capped_grad, _) in zip(gvs, capped_gvs):
+		if grad is not None:
+			tf.summary.histogram('grad/{}'.format(var.name), capped_grad)
+			tf.summary.histogram('capped_fraction/{}'.format(var.name), tf.nn.zero_fraction(grad - capped_grad))
+			tf.summary.histogram('variable/{}'.format(var.name), var)
+
+	merged = tf.summary.merge_all()
+	train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/train')
+	test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test')
+	valid_writer = tf.summary.FileWriter(FLAGS.log_dir + '/valid')
+
+	# Initialize the variables (i.e. assign their default value)
+	init = tf.global_variables_initializer()
+
+	# Start training
+	with tf.Session() as sess:
+
+		# Run the initializer
+		sess.run(init)
+
+		for step in range(1, FLAGS.max_steps + 1):
+			batch_x, batch_y = mnist.train.next_batch(batch_size)
+			# Reshape data to get 28 seq of 28 elements
+			batch_x = batch_x.reshape((batch_size, timesteps, num_input))
+			# Run optimization op (backprop)
+			sess.run(
+				[train_op], feed_dict={X: batch_x, Y: batch_y, training: True})
+			if step % display_step == 0 or step == 1:
+				# Calculate batch loss and accuracy
+				summary, loss, acc, _ = sess.run(
+					[merged, loss_op, accuracy, train_op],
+					feed_dict={X: batch_x, Y: batch_y, training: True})
+				print("Step " + str(step) + ", Minibatch Loss= " + "{:.4f}".format(loss) +
+				      ", Training Accuracy= " + "{:.3f}".format(acc))
+				train_writer.add_summary(summary, step)
+
+				valid_x, valid_y = mnist.validation.next_batch(batch_size)
+				valid_x = valid_x.reshape((batch_size, timesteps, num_input))
+				# Calculate batch loss and accuracy
+				summary, loss, acc = sess.run(
+					[merged, loss_op, accuracy],
+					feed_dict={X: valid_x, Y: valid_y, training: False})
+				print("Step " + str(step) + ", Minibatch Loss= " + "{:.4f}".format(loss) +
+				      ", Valid Accuracy= " + "{:.3f}".format(acc))
+				valid_writer.add_summary(summary, step)
+
+		print("Optimization Finished!")
+
+		print("Validation Finished!")
+
+		test_loss, test_acc = 0, 0
+		for step in range(1, 1000):
+			test_data, test_label = mnist.test.next_batch(batch_size)
+			# Reshape data to get 28 seq of 28 elements
+			test_data = test_data.reshape((batch_size, timesteps, num_input))
+			# Calculate batch loss and accuracy
+			summary, loss, acc = sess.run(
+				[merged, loss_op, accuracy], feed_dict={X: test_data, Y: test_label, training: False})
+			test_loss += loss
+			test_acc += acc
+			print("Step " + str(step) + ", Minibatch Loss= " + "{:.4f}".format(
+				loss) + ", Testing Accuracy= " + "{:.3f}".format(acc))
+			test_writer.add_summary(summary, step)
+
+		print("Test Finished!")
+		print("Test Ave_loss: " + str(test_loss / (1000)))
+		print("Test Ave_acc: " + str(test_acc / (1000)))
+
+		train_writer.close()
+		test_writer.close()
+
+
+def main(_):
+	if tf.gfile.Exists(FLAGS.log_dir):
+		tf.gfile.DeleteRecursively(FLAGS.log_dir)
+	tf.gfile.MakeDirs(FLAGS.log_dir)
+	run()
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--max_steps',
+		type=int,
+		default=15000,
+		help='Number of steps to run trainer.')
+	parser.add_argument(
+		'--lr', type=float, default=0.001, help='Learning rate')
+	parser.add_argument(
+		'--data_dir',
+		type=str,
+		default='../../data/mnist',
+		help='Directory for storing input data')
+	parser.add_argument(
+		'--log_dir',
+		type=str,
+		default='/tmp/logs/mnist/',
+		help='Summaries log directory')
+	parser.add_argument('--cell', type=str, default='bn_sep', help='RNN Cell')
+
+	FLAGS, unparsed = parser.parse_known_args()
+	tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
---
+++ src/normal_cells/lstm_ln_sep.py
@@ -0,0 +1,159 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.ops.rnn_cell_impl import RNNCell, LSTMStateTuple
+
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.util import nest
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.platform import tf_logging as logging
+
+_BIAS_VARIABLE_NAME = "bias"
+_WEIGHTS_VARIABLE_NAME = "kernel"
+
+
+class LNLSTMCell(RNNCell):
+	def __init__(self,
+	             num_units,
+	             forget_bias=1.0,
+	             state_is_tuple=True,
+	             activation=None,
+	             reuse=None):
+
+		super(LNLSTMCell, self).__init__(_reuse=reuse)
+		if not state_is_tuple:
+			logging.warn(
+				"%s: Using a concatenated state is slower and will soon be "
+				"deprecated.  Use state_is_tuple=True.", self)
+		self._num_units = num_units
+		self._forget_bias = forget_bias
+		self._state_is_tuple = state_is_tuple
+		self._activation = activation or math_ops.tanh
+
+	@property
+	def state_size(self):
+		return (LSTMStateTuple(self._num_units, self._num_units)
+		        if self._state_is_tuple else 2 * self._num_units)
+
+	@property
+	def output_size(self):
+		return self._num_units
+
+	def call(self, inputs, state):
+		"""Long short-term memory cell (LSTM)."""
+		sigmoid = math_ops.sigmoid
+		# Parameters of gates are concatenated into one multiply for efficiency.
+		if self._state_is_tuple:
+			c, h = state
+		else:
+			c, h = array_ops.split(value=state, num_or_size_splits=2, axis=1)
+
+		concat = _line_sep([inputs, h], 4 * self._num_units, bias=False)
+
+		# i = input_gate, j = new_input, f = forget_gate, o = output_gate
+		i, j, f, o = array_ops.split(
+			value=concat, num_or_size_splits=4, axis=1)
+
+		new_c = (c * sigmoid(f + self._forget_bias) +
+		         sigmoid(i) * self._activation(j))
+		bn_new_c = layer_norm(new_c, scope='c')
+		new_h = self._activation(bn_new_c) * sigmoid(o)
+
+		if self._state_is_tuple:
+			new_state = LSTMStateTuple(new_c, new_h)
+		else:
+			new_state = array_ops.concat([new_c, new_h], 1)
+		return new_h, new_state
+
+
+def _line_sep(args,
+              output_size,
+              bias,
+              bias_initializer=None,
+              kernel_initializer=None):
+	if args is None or (nest.is_sequence(args) and not args):
+		raise ValueError("`args` must be specified")
+	if not nest.is_sequence(args):
+		args = [args]
+
+	# Calculate the total size of arguments on dimension 1.
+	total_arg_size = 0
+	shapes = [a.get_shape() for a in args]
+	for shape in shapes:
+		if shape.ndims != 2:
+			raise ValueError("linear is expecting 2D arguments: %s" % shapes)
+		if shape[1].value is None:
+			raise ValueError("linear expects shape[1] to \
+                             be provided for shape %s, "
+			                 "but saw %s" % (shape, shape[1]))
+		else:
+			total_arg_size += shape[1].value
+
+	dtype = [a.dtype for a in args][0]
+
+	# Now the computation.
+	scope = vs.get_variable_scope()
+	with vs.variable_scope(scope) as outer_scope:
+
+		[x, h] = args
+		x_size = x.get_shape().as_list()[1]
+		W_xh = tf.get_variable(
+			'W_xh', [x_size, output_size],
+			initializer=tf.orthogonal_initializer)
+		W_hh = tf.get_variable(
+			'W_hh', [int(output_size / 4), output_size],
+			initializer=identity_initializer(0.9))
+
+		xh = tf.matmul(x, W_xh)
+		hh = tf.matmul(h, W_hh)
+
+		ln_xh = layer_norm(xh, scope='ln_xh')
+		ln_hh = layer_norm(hh, scope='ln_hh')
+		res = ln_xh + ln_hh
+
+		if not bias:
+			return res
+		with vs.variable_scope(outer_scope) as inner_scope:
+			inner_scope.set_partitioner(None)
+			if bias_initializer is None:
+				bias_initializer = init_ops.constant_initializer(
+					0.0, dtype=dtype)
+			biases = vs.get_variable(
+				_BIAS_VARIABLE_NAME, [output_size],
+				dtype=dtype,
+				initializer=bias_initializer)
+		return nn_ops.bias_add(res, biases)
+
+
+# LN funcition
+def layer_norm(inputs, epsilon=1e-7, scope=None):
+	# TODO: may be optimized
+	mean, var = tf.nn.moments(inputs, [1], keep_dims=True)
+	with tf.variable_scope(scope + 'LN'):
+		scale = tf.get_variable(
+			'alpha',
+			shape=[inputs.get_shape()[1]],
+			initializer=tf.constant_initializer(1.0))
+		shift = tf.get_variable(
+			'beta',
+			shape=[inputs.get_shape()[1]],
+			initializer=tf.zeros_initializer)
+	LN = scale * (inputs - mean) / tf.sqrt(var + epsilon) + shift
+
+	return LN
+
+
+def identity_initializer(scale):
+	def _initializer(shape, dtype=tf.float32, partition_info=None):
+		size = shape[0]
+		# gate (j) is identity
+		t = np.zeros(shape)
+		t[:, size:size * 2] = np.identity(size) * scale
+		t[:, :size] = np.identity(size) * scale
+		t[:, size * 2:size * 3] = np.identity(size) * scale
+		t[:, size * 3:] = np.identity(size) * scale
+		return tf.constant(t, dtype)
+
+	return _initializer
