2b47347c8af3e5171b7a0995e2fe2b3eab28e0e5
jake <jakewill@qq.com>
2017-11-07 10:57:05 +0800

    fix bn_sep

--- src/ptb/ptb_word_lm.py
+++ src/ptb/ptb_word_lm.py
@@ -90,18 +90,18 @@ flags.DEFINE_string(
 	"A type of model. Possible options are: small, medium, large.")
 flags.DEFINE_string("data_path", "../../data/simple-examples/data/",
                     "Where the training/test data is stored.")
-flags.DEFINE_string("save_path", "/tmp/log/ptb/cn", "Model output directory.")
+flags.DEFINE_string("save_path", "/tmp/log/ptb/bn", "Model output directory.")
 flags.DEFINE_bool("use_fp16", False,
                   "Train using 16-bit floats instead of 32bit floats")
 flags.DEFINE_integer("num_gpus", 1,
                      "If larger than 1, Grappler AutoParallel optimizer "
                      "will create multiple training replicas with each GPU "
                      "running one replica.")
-flags.DEFINE_string("rnn_mode", 'cn_sep',
+flags.DEFINE_string("rnn_mode", 'bn_sep',
                     "The low level implementation of lstm cell: one of CUDNN, "
                     "BASIC, and BLOCK, representing cudnn_lstm, basic_lstm, "
                     "and lstm_block_cell classes.")
-flags.DEFINE_float("lr", 0.01, "learning rate")
+flags.DEFINE_float("lr", 0.0001, "learning rate")
 
 FLAGS = flags.FLAGS
 BASIC = "basic"
